a5f2f26d0d8ecb065180f7d719b9430d7dbdd3eb
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2026-04-20 18:21:08 +0200

    Fix diagnostic LED to use rx_led, bump voicecard to v2.2

--- voicecard/voice.cc
+++ voicecard/voice.cc
@@ -20,6 +20,7 @@
 #include "voicecard/voice.h"
 
 #include "voicecard/audio_out.h"
+#include "voicecard/leds.h"
 #include "voicecard/oscillator.h"
 #include "voicecard/sub_oscillator.h"
 #include "voicecard/transient_generator.h"
@@ -490,8 +491,24 @@ inline void Voice::RenderOscillators() {
   base_pitch += (dst_[MOD_DST_OSC_1_2_COARSE] - 8192) >> 4;
   base_pitch += (dst_[MOD_DST_OSC_1_2_FINE] - 8192) >> 7;
 
-  // Reset engine state when engine type changes.
+  // Diagnostic: rx_led shows engine type (note_led is used by note handler).
+  // classic=off, FM=on, KS=slow blink, WC=fast blink
   uint8_t engine = patch_.padding[2];
+  {
+    static uint8_t diag_counter = 0;
+    ++diag_counter;
+    if (engine == ENGINE_FM4OP) {
+      rx_led.High();
+    } else if (engine == ENGINE_KS_PLUCK) {
+      if (diag_counter & 0x40) rx_led.High(); else rx_led.Low();
+    } else if (engine == ENGINE_WESTCOAST) {
+      if (diag_counter & 0x10) rx_led.High(); else rx_led.Low();
+    } else {
+      rx_led.Low();
+    }
+  }
+
+  // Reset engine state when engine type changes.
   if (engine != last_engine_) {
     last_engine_ = engine;
     fm4op_.Init();
